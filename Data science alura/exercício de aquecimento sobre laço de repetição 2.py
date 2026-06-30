2297332291394f749bee068566d3037e4177d970
pedrosampaiom2007-gif <pedrosampaiom2007@gmail.com>
2026-06-30 17:18:33 -0300

    Delete Data science alura/exercício de aquecimento sobre laço de repetição 2.py

--- Data science alura/exercício de aquecimento sobre laço de repetição 2.py
+++
@@ -1,9 +0,0 @@
-coloniaA= 4
-coloniaB= 10
-dias=0
-while coloniaB>=coloniaA:
-     coloniaA+= float(0.03 * coloniaA)
-     coloniaB+=float(0.015 * coloniaB)
-     dias+=1
-
-print(f"A colônia A levou {dias} dias para superar a colônia B")
